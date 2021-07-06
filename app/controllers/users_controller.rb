8d6624a1757b593fa09a7a42a3ce6b95576e72df
qamk <qamking@gmail.com>
2021-07-06 16:29:03 +0100

    Add new routes, organise views with form partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,25 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+
   def create
-    # @user = User.new(username: params[:username], email: params[:email], password: params[:password]) # when params are top level
-    @user = User.new(create_user_params) # params
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password]) # for top-level params
+    @user = User.new(user_params) # params nested within a :user hash
     if @user.save
       redirect_to new_user_path
     else
@@ -15,7 +31,7 @@ class UsersController < ApplicationController
 
   private
 
-  def create_user_params
+  def user_params
     params.require(:user).permit(:username, :email, :password)
   end
 
